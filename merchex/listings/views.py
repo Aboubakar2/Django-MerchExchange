2e46b9af2a62fcca73e700377a0517edc6186aab
Aboubakar2 <alhassaneaboubacar392@gmail.com>
2024-03-18 16:10:14 +0100

    second commit

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,7 +2,7 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import ContactUsForm
+from .forms import ContactUsForm, BandForm, ListingForm
 from .models import Band, Listing
 
 
@@ -17,6 +17,53 @@ def band_detail(request, id):
     band = Band.objects.get(id=id)
     return render(request,
                   'listings/band_detail.html',
+                  {'band': band})
+
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm()
+
+    return render(request,
+                  'listings/band_create.html',
+                  {'form': form})
+
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = BandForm(request.POST, instance=band)
+        if form.is_valid():
+            # mettre à jour le groupe existant dans la base de données
+            form.save()
+            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+            return redirect('band-detail', band.id)
+    else:
+        form = BandForm(instance=band)
+
+    return render(request,
+                  'listings/band_update.html',
+                  {'form': form})
+
+
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        band.delete()
+        # rediriger vers la liste des groupes
+        return redirect('band-list')
+
+        # pas besoin de « else » ici. Si c'est une demande GET, continuez simplement
+
+    return render(request,
+                  'listings/band_delete.html',
                   {'band': band})
 
 
@@ -57,6 +104,55 @@ def listings(request):
 def listing_detail(request, id):
     listing = Listing.objects.get(id=id)
     return render(request, 'listings/listing_detail.html', {'listing': listing})
+
+
+def listing_create(request):
+    if request.method == 'POST':
+        form = ListingForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+
+    else:
+        form = ListingForm()
+
+    return render(request,
+                  'listings/listing_create.html',
+                  {'form': form})
+
+
+def listing_update(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance=listing)
+        if form.is_valid():
+            # mettre à jour le groupe existant dans la base de données
+            form.save()
+            # rediriger vers la page détaillée du groupe que nous venons de mettre à jour
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListingForm(instance=listing)
+
+    return render(request,
+                  'listings/listing_update.html',
+                  {'form': form})
+
+
+def listing_delete(request, id):
+    listing = Listing.objects.get(id=id)
+
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        listing.delete()
+        # rediriger vers la liste des groupes
+        return redirect('listings')
+
+        # pas besoin de « else » ici. Si c'est une demande GET, continuez simplement
+
+    return render(request,
+                  'listings/listing_delete.html',
+                  {'listing': listing})
 
 
 def email_sent(request):
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -24,10 +24,19 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/', views.band_list, name="band-list"),
     path('bands/<int:id>/', views.band_detail, name="band-detail"),
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:id>/', views.band_detail, name='band-detail'),
+    path('bands/add/', views.band_create, name='band-create'),
+    path('bands/<int:id>/change', views.band_update, name="band-update"),
+    path('bands/<int:id>/delete', views.band_delete, name="band-delete"),
     path('about-us/', views.about, name="about-us"),
     path('contact-us/', views.contact, name="contact"),
     path('listings/', views.listings, name="listings"),
     path('listings/<int:id>/', views.listing_detail, name="listing-detail"),
+    path('listings/add/', views.listing_create, name="listing-create"),
+    path('listings/<int:id>/change', views.listing_update, name="listing-update"),
+    path('listings/<int:id>/delete', views.listing_delete, name="listing-delete"),
+
     path('email_sent/', views.email_sent, name="email-sent"),
 
 ]
